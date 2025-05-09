de78074fe6925d4d56f74dc0bc46d0d36ac3fc6b
Avantemaps-pi <avantemaps.pi@gmail.com>
2025-05-09 14:04:51 +0200

    Update fix-leaflet-icons.ts

--- src/lib/fix-leaflet-icons.ts
+++ src/lib/fix-leaflet-icons.ts
@@ -1,9 +1,14 @@
-// src/fix-leaflet-icons.ts
+// src/lib/fix-leaflet-icons.ts
+
 import L from 'leaflet';
+
+// Fix leaflet's default icon paths
+import 'leaflet/dist/leaflet.css';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+// @ts-ignore: Ignore readonly error on Icon.Default.prototype.options
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
