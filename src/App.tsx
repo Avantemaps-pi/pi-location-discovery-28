6ce68207336d27c961f2998cf63b0a7920c3ead5
Avantemaps-pi <avantemaps.pi@gmail.com>
2025-05-09 11:15:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 
+import MapComponent from "./components/MapComponent";
+import { defaultLocations } from "./data/defaultLocations";
 import { useEffect, useState } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
