ae6302e6a460e7dc088dbddbeb854eaf990976b4
Avantemaps-pi <avantemaps.pi@gmail.com>
2025-05-09 11:47:13 +0200

    Update LeafletMap.tsx

--- src/components/map/LeafletMap.tsx
+++ src/components/map/LeafletMap.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';  // Required for custom icons (if used)
+import 'leaflet/dist/leaflet.css';  // Required for Leaflet to work properly
 import { Place } from '@/data/mockPlaces';
 import { toast } from 'sonner';
 import { defaultLocations } from './defaultLocations';
